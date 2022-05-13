1e9292324934e2afd082d66cf49422731da56392
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-13 16:08:45 +0900

    헤더푸터따로나눔

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/styles.css">
     <script src="https://kit.fontawesome.com/8eb4f0837a.js" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
