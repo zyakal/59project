7782f6945abcf8a6acb61725a032d8f6e1b84f8c
eunjiP <dae4227@naver.com>
2022-05-16 17:00:13 +0900

    update

--- db/db.php
+++ db/db.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<?php
-define("URL","localhost");
-define("USERNAME","root");
-define("PASSWORD","506greendg@");
-define("DB_NAME","59project");
-define("PORT","3306");
-
-function get_conn(){
-    mysqli_connect(URL,USERNAME,PASSWORD,DB_NAME,PORT);
-}
-=======
 <?php
 define("URL", "192.168.0.70");
 define("USERNAME", "root");
@@ -21,4 +9,4 @@ function get_conn()
 {
     return mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME, PORT);
 }
->>>>>>> 426d8c2635c6cf531241f5f262c0120815230c3a
+
