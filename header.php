0bbab2196f005a584593ce4d21e5e7c92058f93f
eunjiP <dae4227@naver.com>
2022-05-16 17:11:28 +0900

    update

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav class="header--nav">
     <div class="nav--logo">
         <a href="#" class="nav--back">
@@ -16,22 +15,4 @@
         </a>
     </div>
 </nav>
-=======
-<nav class="header--nav">
-    <div class="nav--logo">
-        <a href="#" class="nav--back">
-            <i class="fa-solid fa-arrow-left"></i>
-        </a>
-    </div>
-    <div class="nav--addr">
-        <a href="#">
-            <!-- 본인페이지에서 $page_name에 페이지이름 문자열로 넣어주기  -->
-            <?= $page_name ?>
-        </a>
-    </div>
-    <div class="nav--notice">
-        <a href="#">
-        </a>
-    </div>
-</nav>
->>>>>>> 4e2d13a221b1a428db3bf07aba49e916c753080b
+
