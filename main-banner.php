a33d91e591cd49153be856f01575b6e8d5215fef
eunjiP <dae4227@naver.com>
2022-05-18 18:07:58 +0900

    banner update

--- main-banner.php
+++ main-banner.php
@@ -15,6 +15,9 @@
         <li class="slide__item">
             <img class="slide__img" src="img/banner_icon/banner_5.jpg">
         </li>
+        <li class="slide__item">
+            <img class="slide__img" src="img/banner_icon/banner_6.jpg">
+        </li>
     </ul>
     <div class="paginations"></div>
 </div>
