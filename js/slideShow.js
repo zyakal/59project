aab9c7c18516026747ba1e92dedd7f3ee1a48c75
eunjiP <dae4227@naver.com>
2022-05-18 10:12:37 +0900

    slide update

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,19 +1,117 @@
-var slideIndex = 0;
-showSlides();
+const list = document.querySelector('.slide__list');
+const items = document.querySelectorAll('.slide__item');
+const paginations = document.querySelector('.paginations');
+const lastIndex = items.length - 1;
+let selected = 0;
+let interval;
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("slide_item");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
+// Util Functions
+const setTransition = (value) => {
+    list.style.transition = value;
+};
+
+const setTranslate = ({ index, reset }) => {
+    if (reset) list.style.transform = `translate(-${list.clientWidth}px, 0)`;
+    else
+        list.style.transform = `translate(-${
+            (index + 1) * list.clientWidth
+        }px, 0)`;
+};
+
+const activePagination = (index) => {
+    [...paginations.children].forEach((pagination) => {
+        pagination.classList.remove('on');
+    });
+    paginations.children[index].classList.add('on');
+};
+
+// Make the pagination buttons.
+const handlePagination = (e) => {
+    if (e.target.dataset.num) {
+        selected = parseInt(e.target.dataset.num);
+        setTransition('all 0.3s linear');
+        setTranslate({ index: selected });
+        activePagination(selected);
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+};
+
+const makePagination = () => {
+    if (items.length > 1) {
+        for (let i = 0; i < items.length; i++) {
+            const button = document.createElement('button');
+            button.dataset.num = i;
+            button.classList.add('pagination');
+            if (i === 0) {
+                button.classList.add('on');
+            }
+            paginations.appendChild(button);
+            paginations.addEventListener('click', handlePagination);
+        }
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+};
+
+// Make prev and next buttons.
+const handlePrev = () => {
+    selected -= 1;
+    setTransition('transform 0.3s linear');
+    setTranslate({ index: selected });
+    if (selected < 0) {
+        selected = lastIndex;
+        setTimeout(() => {
+            setTransition('');
+            setTranslate({ index: selected });
+        }, 300);
+    }
+    if (selected >= 0) activePagination(selected);
+};
+
+const handleNext = () => {
+    console.log(selected);
+    selected += 1;
+    setTransition('transform 0.3s linear');
+    setTranslate({ index: selected });
+    if (selected > lastIndex) {
+        selected = 0;
+        setTimeout(() => {
+            setTransition('');
+            setTranslate({ index: selected });
+        }, 300);
+    }
+    if (selected <= lastIndex) activePagination(selected);
+};
+
+// Clone the first and last elements.
+const cloneElement = () => {
+    list.prepend(items[lastIndex].cloneNode(true));
+    list.append(items[0].cloneNode(true));
+    setTranslate({ reset: true });
+};
+
+// Automatically play the slide
+const autoplayIterator = () => {
+    selected += 1;
+    setTransition('all 0.3s linear');
+    setTranslate({ index: selected });
+    if (selected > lastIndex) {
+        activePagination(0);
+        clearInterval(interval);
+        setTimeout(() => {
+            selected = 0;
+            setTransition('');
+            setTranslate({ reset: true });
+            autoplay({ duration: 2000 });
+        }, 300);
+    }
+    if (selected <= lastIndex) activePagination(selected);
+};
+
+const autoplay = ({ duration }) => {
+    interval = setInterval(autoplayIterator, duration);
+};
+
+const render = () => {
+    makePagination();
+    cloneElement();
+    autoplay({ duration: 2000 });
+};
+render();
--- main-banner.php
+++ main-banner.php
@@ -1,31 +1,20 @@
-<div class="slide_box">
-    <div class="slide_list">
-        <div class="slide_item fade">
-            <div class="slide_num" hidden>1 / 5</div>
-            <img src="../img/가죽공예.jpg">
-        </div>
-        <div class="slide_item fade">
-            <div class="slide_num" hidden>2 / 5</div>
-            <img src="../img/네일샵.jpg">
-        </div>
-        <div class="slide_item fade">
-            <div class="slide_num" hidden>3 / 5</div>
-            <img src="../img/도넛.jpg">
-        </div>
-        <div class="slide_item fade">
-            <div class="slide_num" hidden>4 / 5</div>
-            <img src="../img/미용실.jpg">
-        </div>
-        <div class="slide_item fade">
-            <div class="slide_num" hidden>5 / 5</div>
-            <img src="../img/카페.jpg">
-        </div>
-    </div>
-    <div class="slide_btn_box">
-        <span class="dot"></span> 
-        <span class="dot"></span> 
-        <span class="dot"></span> 
-        <span class="dot"></span> 
-        <span class="dot"></span> 
-    </div>
+<div class="slide__box">
+    <ul class="slide__list">
+        <li class="slide__item">
+            <img class="slide__img" src="../img/가죽공예.jpg">
+        </li>
+        <li class="slide__item">
+            <img class="slide__img" src="../img/네일샵.jpg">
+        </li>
+        <li class="slide__item">
+            <img class="slide__img" src="../img/도넛.jpg">
+        </li>
+        <li class="slide__item">
+            <img class="slide__img" src="../img/미용실.jpg">
+        </li>
+        <li class="slide__item">
+            <img class="slide__img" src="../img/카페.jpg">
+        </li>
+    </ul>
+    <div class="paginations"></div>
 </div>
