016794d24104d88b8d9edf1ac1657e656ab4860a
eunjiP <dae4227@naver.com>
2022-05-19 12:19:44 +0900

    store detail page 이동

--- store_list_form.php
+++ store_list_form.php
@@ -1,14 +1,16 @@
-<div class="list__item">
-    <div class="list__store__img"><img src="img/store/<?=$row['store_nm']?>/Main_img/<?=$row['store_photo']?>"></div>
-    <div class="list__store__info">
-        <div class="store__info__nm"><?=$row['store_nm']?></div>
-        <div class="store__info__info">가게 정보</div>
-        <?php 
-        if (isset($star)) { ?>
-            <div class='store__info__star_rating'><i class='fa-solid fa-star'></i></div>
-        <?php } else { ?>
-            
-        <?php } ?>
+<a href="store-detail.php?store_num=<?=$row['store_num']?>">
+    <div class="list__item">
+        <div class="list__store__img"><img src="img/store/<?=$row['store_nm']?>/Main_img/<?=$row['store_photo']?>"></div>
+        <div class="list__store__info">
+            <div class="store__info__nm"><?=$row['store_nm']?></div>
+            <div class="store__info__info">가게 정보</div>
+            <?php 
+            if (isset($star)) { ?>
+                <div class='store__info__star_rating'><i class='fa-solid fa-star'></i></div>
+            <?php } else { ?>
+                
+            <?php } ?>
+        </div>
+        <div class="list__store__location"><i class="fa-solid fa-location-dot"></i> 1.0 KM</div>
     </div>
-    <div class="list__store__location"><i class="fa-solid fa-location-dot"></i> 1.0 KM</div>
-</div>
+</a>
