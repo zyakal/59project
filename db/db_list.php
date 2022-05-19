d539cb93dee9ac0af7ac402d4160b00ab3fcaac9
eunjiP <dae4227@naver.com>
2022-05-19 14:42:38 +0900

    likestore page

--- db/db_list.php
+++ db/db_list.php
@@ -1,6 +1,7 @@
 <?php
     include_once "db/db.php";
 
+    //list page - 전체 가게
     function sel_store_list() {
         $conn = get_conn();
         $sql = "SELECT store_nm, store_photo, store_num FROM t_store";
@@ -9,6 +10,7 @@
         return $result;
     }
 
+    //list page - 별점구하는 함수
     function store_star(&$param) {
         $store_num = $param['store_num'];
         $conn = get_conn();
@@ -20,6 +22,7 @@
         return $result;
     }
 
+    //list page - 카테고리 들고오는 함수
     function cate_name(&$param) {
         $store_num = $param['store_num'];
         $conn = get_conn();
@@ -28,6 +31,21 @@
         ON A.cate_num = B.cate_num
         WHERE B.store_num = $store_num";
         $result = mysqli_query($conn, $sql);
-        return mysqli_fetch_assoc($result);
+        mysqli_close($conn);
+        return mysqli_fetch_assoc($result);     //복수일 경우 변경 해야함!!
     }
+
+    //likestore page - 찜한 가게 불러오는 함수
+    function sel_like_stores(&$param) {
+        $user_num = $param['user_num'];
+        $conn = get_conn();
+        $sql = "SELECT B.store_nm, B.store_photo, B.store_num FROM t_likestore A
+        INNER JOIN t_store B
+        ON A.store_num = B.store_num
+        WHERE A.user_num = $user_num";
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        return $result;
+    }
+
     
