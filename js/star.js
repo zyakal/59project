21fbbc57c9770e5ad637f56850e7566a4d756d9f
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-25 17:58:20 +0900

    가게페이지 오류 수정

--- js/star.js
+++ js/star.js
@@ -1,5 +1,7 @@
 const storeRating = document.querySelector('.store--rating');
 const ratingPoint = storeRating.querySelector('.ratingPoint').innerHTML;
 const starsInner = storeRating.querySelector('.stars-inner');
-const starPercentage = (parseFloat(ratingPoint) / 5) * 100;
-starsInner.style.width = `${starPercentage}%`;
+if (ratingPoint !== null) {
+  const starPercentage = (parseFloat(ratingPoint) / 5) * 100;
+  starsInner.style.width = `${starPercentage}%`;
+}
